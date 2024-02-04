29f7b2705a99a883458fc53ea941a7910eed81d5
Tang <1463343010@qq.com>
2024-02-05 00:04:09 +0800

    all

--- 2024.2.4/2024.2.4/test.c
+++ 2024.2.4/2024.2.4/test.c
@@ -1,2 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+//x86-32位环境，sizeof返回无符号整型%zu
+//指针类型决定被解引用是访问几个字节,决定指针加1跳过几个字节
+int main()
+{
+	int a = 100;
+	char* b = (char) & a;
+	printf("%d",sizeof((*b)));
+}
